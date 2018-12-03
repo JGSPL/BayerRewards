6ab08cdf1d2e3ee1e95ace1405a905ee2ca26e14
Sneha <sneha@procialize.in>
2018-12-03 18:26:05 +0530

    latest code

--- app/src/main/java/com/procialize/singleevent/Activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/procialize/singleevent/Activity/ForgotPasswordActivity.java
@@ -2,14 +2,19 @@ package com.procialize.singleevent.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.procialize.singleevent.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    ImageView headerlogoIv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        headerlogoIv.setVisibility(View.VISIBLE);
     }
 }
