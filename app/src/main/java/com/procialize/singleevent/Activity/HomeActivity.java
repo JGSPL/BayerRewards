6e11efdb29286e6cf44920588981010b855909a2
Rahul <info@procialize.in>
2018-12-18 14:54:56 +0530

    fixed event info and notification

--- app/src/main/java/com/procialize/singleevent/Activity/HomeActivity.java
+++ app/src/main/java/com/procialize/singleevent/Activity/HomeActivity.java
@@ -145,7 +145,7 @@ public class HomeActivity extends AppCompatActivity implements CustomMenuAdapter
     String eventid, token;
     String email, password;
     CustomMenuAdapter customMenuAdapter;
-    TextView logout, home, contactus, eventname, switchbt, eula, privacy_policy;
+    TextView logout, home, contactus, eventname, switchbt, eula, privacy_policy,eventInfo,notification;
     String eventnamestr;
     public static final int RequestPermissionCode = 8;
     public static String logoImg = "", colorActive = "";
@@ -459,6 +459,9 @@ public class HomeActivity extends AppCompatActivity implements CustomMenuAdapter
         contactus = navigationView.findViewById(R.id.contactus);
         switchbt = navigationView.findViewById(R.id.switchbt);
         privacy_policy = navigationView.findViewById(R.id.privacy_policy);
+        eventInfo = navigationView.findViewById(R.id.eventInfo);
+        notification = navigationView.findViewById(R.id.notification);
+
         eula = navigationView.findViewById(R.id.eula);
 
 //        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -519,6 +522,25 @@ public class HomeActivity extends AppCompatActivity implements CustomMenuAdapter
 
             }
         });
+        eventInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent main = new Intent(getApplicationContext(), EventInfoActivity.class);
+                startActivity(main);
+
+            }
+        });
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent main = new Intent(getApplicationContext(), NotificationActivity.class);
+                startActivity(main);
+
+            }
+        });
+
 
         eula.setOnClickListener(new View.OnClickListener() {
             @Override
